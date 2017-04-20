6b0e226e357a9f06ef2cd30b986aec65f1fd4121
Jeremy Tribby <jeremy@tribby.com>
2017-04-20 02:05:46 -0700

    Add brace layer glyphs script

---
+++ tools/Brace Layer Sidebearings.py
@@ -0,0 +1,32 @@
+#MenuTitle: Brace Layer Sidebearings Re-Interpolate
+# -*- coding: utf-8 -*-
+
+__doc__="""
+Re-interpolate sidebearings of brace layers
+"""
+
+import GlyphsApp, re
+
+font = Glyphs.font
+brace = re.compile("^{\s*\d+,\s*\d+\s*}$")
+
+for glyph in font.glyphs:
+  if glyph.layers > 1 and '.' not in glyph.name:
+    for layer in glyph.layers:
+      if brace.match(layer.name):
+
+        name = layer.name
+
+        newLayer = layer.copy()
+        layer.name = "tmp"
+        newLayer.name = name
+
+        glyph.layers.append(newLayer)
+
+        newLayer.reinterpolate()
+
+        layer.LSB = newLayer.LSB
+        layer.RSB = newLayer.RSB
+        layer.name = name
+
+        del(glyph.layers[newLayer.layerId])
